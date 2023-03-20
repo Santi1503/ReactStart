5f1acc9d2c6feb44b3b2e702529f23ef01a3228c
Santiago Gomez de la Torre <santiago.gomez1@alumnos.uneatlantico.es>
2023-03-20 23:41:51 +0100

    Start journey

---
+++ main.js
@@ -0,0 +1,10 @@
+alert("Hola Mundo")
+
+var nombre = "Santiago Gómez de la Torre";
+var Años = 20;
+
+var concatenacion = nombre + " " + Años;
+
+var datos = document.getElementById("datos");
+
+datos.innerHTML = concatenacion;
