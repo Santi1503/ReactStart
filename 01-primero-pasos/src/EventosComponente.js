a6a8901d51f974829e3aee7e229426b9d06a88a4
Santiago Gomez de la Torre <santiago.gomez1@alumnos.uneatlantico.es>
2023-03-21 23:55:54 +0100

    Adding more events

--- 01-primero-pasos/src/EventosComponente.js
+++ 01-primero-pasos/src/EventosComponente.js
@@ -10,16 +10,39 @@ export const EventosComponente = () => {
         alert("Dado doble click")
     }
     
+    const hasEntrado = (e, accion) => {
+        console.log("Se ha "+accion+" a la caja")
+    }
+
+    const hasSalido = (e, accion) => {
+        console.log("Se ha "+accion+" a la caja")
+    }
+
+    /*const IntroducirNombre = e => {
+        console.log("Introduce tu nombre");
+    }
+
+    const DejasteTuNombre = e => {
+        console.log("Introduce tu nombre");
+    }*/
+
     return (
     <div>
         <h2>Eventos Componente</h2>
 
         <p><button onClick={e => hasDadoClick(e, "Santiago")}>dame click</button></p>
 
-        <button onDoubleClick={hasDadoDobleClick}>dame  doble click</button>
+        <p><button onDoubleClick={hasDadoDobleClick}>dame  doble click</button></p>
 
-    
-    
+        <div id="caja" onMouseEnter={e => hasEntrado (e, "entrado")}
+                        onMouseLeave={e => hasSalido (e, "salido")}>
+            Pasa por aqui
+        </div>
+
+        <p>
+            <input type="text" placeholder="introduce tu nombre" />
+        </p>
+
     </div>
   )
 }
