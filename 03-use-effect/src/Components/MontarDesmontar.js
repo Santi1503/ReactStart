06150ba43f0e6c2fae3e62b73a0fd8d0efbd18ca
santiago.gomez1 <santiago.gomez1@alumnos.uneatlantico.es>
2023-03-27 08:28:40 +0200

    Update exercise 03

--- 03-use-effect/src/Components/MontarDesmontar.js
+++ 03-use-effect/src/Components/MontarDesmontar.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import logo from '../img/SantiChiquito.jpg'
 
 export const MontarDesmontar = () => {
+
+
+  useEffect (() => {
+    alert("Has encontrado a Santi chiquito")
+
+    return () => {
+      alert("Se fue Santi chiquito")
+    }
+  }, []);
+
   return (
     <div>
         <h2>Santi chiquito</h2>
