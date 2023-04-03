4f4847f9554c1eeaf82bd505defd374bdd8a105a
Santiago Gomez de la Torre <santiago.gomez1@alumnos.uneatlantico.es>
2023-04-03 23:24:34 +0200

    First Form

--- 04-formularios/src/Components/FormularioComponent.js
+++ 04-formularios/src/Components/FormularioComponent.js
@@ -2,8 +2,28 @@ import React from 'react'
 
 export const FormularioComponent = () => {
   return (
-    <div className='title'>
-        <h1>Let The Journey Begin</h1>
+    <div>
+      <h2 className='title'>Let The Journey Begin</h2>
+      
+      <span>
+        <form className='form'>
+          <p>
+            <input className='name' type='text' placeholder='Nombre' />
+          </p>
+          <p>
+            <input className='name' type='text' placeholder='Apellidos' />
+          </p>
+          <p>
+            <select className='gender'>
+              <option value="null">Sexo</option>
+              <option value="male">Hombre</option>
+              <option value="female">Mujer</option>
+            </select>
+          </p>
+          <button className='submit'>Submit</button>
+        </form>
+      </span>
+        
     </div>
   )
 }
