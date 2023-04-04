146f8bfa7106f9a882a19c2a64b8373d95ad14b3
santiago.gomez1 <santiago.gomez1@alumnos.uneatlantico.es>
2023-04-04 10:37:18 +0200

    update

--- 04-formularios/src/Components/FormularioComponent.js
+++ 04-formularios/src/Components/FormularioComponent.js
@@ -1,29 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const FormularioComponent = () => {
+
+  const [usuario, setUsuario] = useState({});
+
+
+  const conseguirDatosForm = e => {
+    e.preventDefault();
+
+    let datos = e.target;
+    let usuario = {
+      nombre: datos.nombre.value,
+      apellidos: datos.apellidos.value,
+      genero: datos.genero.value
+    };
+
+    /*console.log(usuario);*/
+
+    setUsuario(usuario);
+  }
+
   return (
     <div>
       <h2 className='title'>Let The Journey Begin</h2>
       
       <span>
-        <form className='form'>
+        <form className='form' onSubmit={conseguirDatosForm}>
           <p>
-            <input className='name' type='text' placeholder='Nombre' />
+            <input className='name' type='text' placeholder='Nombre' name='nombre'/>
           </p>
           <p>
-            <input className='name' type='text' placeholder='Apellidos' />
+            <input className='name' type='text' placeholder='Apellidos' name='apellidos'/>
           </p>
           <p>
-            <select className='gender'>
+            <select className='gender' name='genero'>
               <option value="null">Sexo</option>
               <option value="male">Hombre</option>
               <option value="female">Mujer</option>
             </select>
           </p>
-          <button className='submit'>Submit</button>
+          <button className='submit' type='submit' value="Enviar">Enviar</button>
         </form>
       </span>
-        
+            
+      {usuario.apellidos && usuario.apellidos.length > 1 && 
+        (
+          <div className='info-user'>
+            <p>Bienvenido al viaje {usuario.nombre} {usuario.apellidos}</p> 
+          </div>
+        )
+      }
+
     </div>
   )
 }
