28f857cdb00494ab046a4d064216209a693abe95
Santiago Gomez de la Torre <santiago.gomez1@alumnos.uneatlantico.es>
2023-05-10 19:33:20 +0200

    New start: Routes

---
+++ 07-rutas/src/Routers/RouterPrincipal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Routes, Route, Link, BrowserRouter} from 'react-router-dom';
+import { Inicio } from '../Components/Inicio';
+import { Articulos } from '../Components/Articulos';
+import { Contacto } from '../Components/Contacto';
+import { MensajeError } from '../Components/Error';
+
+export const RouterPrincipal = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/inicio" element={<Inicio />} />
+            <Route path="/articulos" element={<Articulos />} />
+            <Route path="/contacto" element={<Contacto />} />
+            <Route path="*" element={<MensajeError />} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
