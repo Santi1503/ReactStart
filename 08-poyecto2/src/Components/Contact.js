c76a289edf4472b9091ff756fcbc09446749f5ab
Santiago Gómez de la Torre <santiago.gomez1@alumnos.uneatlantico.es>
2024-02-26 12:02:19 +0100

    Adding CSS

--- 08-poyecto2/src/Components/Contact.js
+++ 08-poyecto2/src/Components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Contact = () => {
   return (
     <div className='page'>
-      <h1>Contacto</h1>
+      <h1 className='heading'>Contacto</h1>
 
       <form className='contact' action="mailto:gomezdelatorre15@gmail.com">
         <input type='text' placeholder='Nombre' required />
--- 08-poyecto2/src/Components/Home.js
+++ 08-poyecto2/src/Components/Home.js
@@ -5,19 +5,19 @@ export const Home = () => {
   return (
     <div className='home'>
       <h1>
-      ¡Hola! Soy Santiago Gómez de la Torre Romero, 
-      un entusiasta principiante en el <strong>desarrollo web</strong>. 
+      Bienvenido, mi nombre es <strong>Santiago Gómez de la Torre Romero</strong>, 
+      soy un entusiasta en el <strong>desarrollo web</strong>. 
       Aunque estoy en los primeros pasos de mi carrera, 
-      cada proyecto es una oportunidad de <strong>aprendizaje y perfeccionamiento</strong>.       
+      cada proyecto es una oportunidad de aprendizaje y perfeccionamiento.       
       </h1>
 
       <h2>
         Estoy a toda dispisición de colaborar en la creación de alguna aplicación web
-        o sitio web, tener mas visibilidad y relevancia en internet. <Link to="/contact">Contacta conmigo</Link>
+        o sitio web <Link to="/contact">Contacta conmigo</Link>
       </h2>
 
       <section className='lastProjects'>
-        <h2>Mis ultimos proyectos</h2>
+        <h2 className='heading'>Mis ultimos proyectos</h2>
         <p>Estos son algunos de mis trabajos de desarrollo web</p>
 
         <div className='projects'>
