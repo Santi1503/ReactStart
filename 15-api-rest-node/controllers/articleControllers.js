bb305e97ca9202271bd6d9734928a516ca675062
santiago.gomez1 <santiago.gomez1@alumnos.uneatlantico.es>
2024-11-13 14:58:36 +0100

    Adding upload image

--- 15-api-rest-node/controllers/articleControllers.js
+++ 15-api-rest-node/controllers/articleControllers.js
@@ -1,5 +1,8 @@
 const validator = require("validator")
 const Article = require("../models/Article")
+const fs = require("fs")
+const { error } = require("console")
+const path = require("path")
 
 const test = (req, res) => {
     return res.status(200).json({
@@ -103,10 +106,187 @@ const one = (req, res) => {
         });
 }
 
+const deleteArticle = (req, res) => {
+    let id = req.params.id;
+    Article.findByIdAndDelete(id)
+       .then((article) => {
+            if (!article) {
+                return res.status(404).json({
+                    status: "error",
+                    message: "Artículo no encontrado"
+                });
+            }
+            return res.status(200).json({
+                status: "success",
+                article,
+                message: "Artículo eliminado correctamente"
+            });
+        })
+       .catch((error) => {
+            return res.status(400).json({
+                status: "error",
+                message: "Error al eliminar el artículo",
+                error
+            });
+        });
+}
+
+const validateArticle = async (parametros, req, res, id) => {
+    try {
+        let validateTitle = !validator.isEmpty(parametros.title) && validator.isLength(parametros.title, {min: 5, max: undefined})
+        let validateContent = !validator.isEmpty(parametros.content)
+
+        if (!validateTitle || !validateContent) {
+            throw new Error("No se ha validado la información")
+        }
+
+        const article = await Article.findByIdAndUpdate(id, parametros, {new: true})
+
+        return res.status(200).json({
+            status: 'success',
+            article,
+            message: "Artículo editado correctamente"
+        })
+    } catch(error) {
+        return res.status(500).json({
+            status: "error",
+            message: "Error al editar el artículo",
+            error
+        })
+    }
+}
+
+const edit = (req, res) => {
+    let id = req.params.id;
+    let parametros = req.body;
+
+    validateArticle(parametros, req, res, id)
+    
+}
+
+const upload = async (req, res) => {
+    const extension = req.file.filename.split('.').pop().toLowerCase();
+
+    if (!["png", "jpg", "jpeg", "gif"].includes(extension)) {
+        fs.unlink(req.file.path, (err) => {
+            if (err) console.error("Error al eliminar el archivo:", err);
+        });
+        return res.status(400).json({
+            status: "error",
+            message: "La extensión del archivo no es válida"
+        });
+    } else {
+        try {
+            let id = req.params.id;
+
+            // Actualizamos el artículo con la nueva imagen
+            const updatedArticle = await Article.findOneAndUpdate(
+                { _id: id },
+                { image: req.file.filename },
+                { new: true }
+            );
+
+            if (!updatedArticle) {
+                return res.status(400).json({
+                    status: "error",
+                    message: "Error al actualizar el artículo"
+                });
+            }
+
+            return res.status(200).json({
+                status: "success",
+                message: "Imagen subida correctamente",
+                file: req.file
+            });
+        } catch (error) {
+            return res.status(500).json({
+                status: "error",
+                message: "Error al actualizar el artículo",
+                error
+            });
+        }
+    }
+};
+
+const image = (req, res) => {
+    let fichero = req.params.fichero
+    let pathFichero = path.resolve(
+        __dirname,
+        "..",
+        "images",
+        "articles",
+        fichero
+    )
+
+    fs.access(pathFichero, fs.constants.F_OK, (error) => {
+        if (!error) {
+            return res.sendFile(pathFichero)
+        } else {
+            return res.status(404).json({
+                status: "error",
+                message: "Imagen no encontrada",
+                error,
+                fichero,
+                pathFichero
+            })
+        }
+    })
+}
+
+const searchArticle = (req, res) => {
+    let search = req.params.search;
+
+    // Validar que el parámetro de búsqueda sea una cadena
+    if (!search || typeof search !== "string" || search.trim() === "") {
+        return res.status(400).json({
+            status: "error",
+            message: "El parámetro de búsqueda es inválido o está vacío"
+        });
+    }
+
+    Article.find({
+        "$or": [
+            { "title": { "$regex": search, "$options": "i" } },
+            { "content": { "$regex": search, "$options": "i" } }
+        ]
+    })
+    .sort({ createdAt: -1 })
+    .then(articlesFound => {
+        // Verificar si no se encontraron artículos
+        if (!articlesFound || articlesFound.length === 0) {
+            return res.status(404).json({
+                status: "error",
+                message: "No se encontraron artículos que coincidan con la búsqueda"
+            });
+        }
+
+        // Devolver los artículos encontrados
+        return res.status(200).json({
+            status: "success",
+            articles: articlesFound,
+            message: "Artículos encontrados correctamente"
+        });
+    })
+    .catch(error => {
+        // Manejo de errores
+        console.error("Error al buscar artículos:", error);
+        return res.status(500).json({
+            status: "error",
+            message: "Error al buscar los artículos",
+            error
+        });
+    });
+};
+
 module.exports = {
     test,
     cursos,
     crear,
     list,
-    one
+    one,
+    deleteArticle,
+    edit,
+    upload,
+    image,
+    searchArticle
 }
--- 15-api-rest-node/routes/articleRoute.js
+++ 15-api-rest-node/routes/articleRoute.js
@@ -1,6 +1,29 @@
 const express = require("express")
 const router = express.Router()
+const multer = require("multer")
 const ArticleController = require("../controllers/articleControllers")
+const path = require("path")
+
+const articlesStorage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './images/articles/')
+    },
+    filename: function(req, file, cb) {
+        cb(null, "article_" + Date.now() + path.extname(file.originalname))
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    const allowedExtensions = ["png", "jpg", "jpeg", "gif"]
+    const fileExtension = path.extname(file.originalname).toLowerCase().slice(1)
+
+    if (!allowedExtensions.includes(fileExtension)) {
+        return cb(new Error("Only images are allowed"), false)
+    }
+
+    cb(null, true)
+}
+const uploads = multer({ storage: articlesStorage, fileFilter: fileFilter })
 
 // Rutas de pruebas
 router.get("/ruta-test", ArticleController.test)
@@ -10,7 +33,10 @@ router.get("/cursos", ArticleController.cursos)
 router.post("/crear", ArticleController.crear)
 router.get("/articulos/:ultimos?", ArticleController.list)
 router.get("/articulo/:id", ArticleController.one)
-
+router.delete("/articulo/:id", ArticleController.deleteArticle)
+router.put("/articulo/:id", ArticleController.edit)
+router.post("/subir-imagen/:id", [uploads.single("file0")], ArticleController.upload)
+router.get("/imagen/:fichero", ArticleController.image)
+router.get("/search/:search", ArticleController.searchArticle)
 
 module.exports = router
-
